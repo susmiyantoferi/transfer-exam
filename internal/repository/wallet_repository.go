7fb2b49da827d33c416ceb3aa76f1ae6fe1e0bfb
Feri Susmiyanto <irefmcr@gmail.com>
2026-09-11 00:05:55 +0700

    not useable repo

--- internal/repository/wallet_repository.go
+++ internal/repository/wallet_repository.go
@@ -2,13 +2,10 @@ package repository
 
 import (
 	"transfer-exam/internal/entity"
-
-	"gorm.io/gorm"
 )
 
 type WalletRepository interface {
 	Repository[entity.Wallet]
-	GetUserAndCurrency(db *gorm.DB, userID, currency string) (*entity.Wallet, error)
 }
 
 type walletRepositoryImpl struct {
@@ -17,9 +14,4 @@ type walletRepositoryImpl struct {
 
 func NewWalletRepositoryImpl() WalletRepository {
 	return &walletRepositoryImpl{}
-}
-
-func (w *walletRepositoryImpl) GetUserAndCurrency(db *gorm.DB, userID, currency string) (*entity.Wallet, error) {
-	var wallet entity.Wallet
-	return &wallet, db.Where("user_id = ? AND currency = ?", userID, currency).Take(&wallet).Error
 }
